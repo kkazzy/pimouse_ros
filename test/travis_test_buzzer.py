595dbdec154f41da93a8852213faf46ed3c78a19
Kazuyuki Kawazu <knz_soft@yahoo.co.jp>
2018-02-04 19:00:05 +0900

    buzzer4.py 8.3.4 travis_test_buzzer-02

--- test/travis_test_buzzer.py
+++ test/travis_test_buzzer.py
@@ -4,7 +4,7 @@ import rospy, unittest, rostest, actionlib
 import rosnode
 import time
 from std_msgs.msg import UInt16
-from pimouse_ros.msg import MusicAction, MusicResult, MUsicFeedback, MusicGoal
+from pimouse_ros.msg import MusicAction, MusicResult, MusicFeedback, MusicGoal
 
 class BuzzerTest(unittest.TestCase):
     def setUp(self):
