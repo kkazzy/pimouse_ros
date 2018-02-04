b73e8432e22432b54260e807488b643b2b6e3be5
Kazuyuki Kawazu <knz_soft@yahoo.co.jp>
2018-02-04 18:55:05 +0900

    buzzer4.py 8.3.4 travis_test_buzzer

--- test/travis_test_buzzer.py
+++ test/travis_test_buzzer.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
-import rospy, unittest, rostest
+#encoding: utf8
+import rospy, unittest, rostest, actionlib
 import rosnode
 import time
 from std_msgs.msg import UInt16
+from pimouse_ros.msg import MusicAction, MusicResult, MUsicFeedback, MusicGoal
 
 class BuzzerTest(unittest.TestCase):
+    def setUp(self):
+        self.client = actionlib.SimpleActionClient("music", MusicAction)
+        self.device_values = []
+        # デバイスファイルに書き出された値を採取しておくリスト
+ 
     def test_node_exist(self):
         nodes = rosnode.get_node_names()
         self.assertIn('/buzzer', nodes, "node does not exist")
@@ -18,6 +25,21 @@ class BuzzerTest(unittest.TestCase):
         with open("/dev/rtbuzzer0", "r") as f:
             data = f.readline()
             self.assertEqual( data, "1234\n", "value does not written to rtbuzzer0") 
+
+    def test_music(self):
+        goal = MusicGoal()
+        goal.freqs = [100,200,300,0]
+        goal.durations = [2,2,2,2]
+
+        self.client.wait_for_server()
+        self.client.send_goal( goal, feedback_cb=self.feedback_cb)
+        self.client.wait_for_result()
+
+
+    def feedback_cb(self):
+        with open("/dev/rtbuzzer0", "r") as f:
+            data = f.readline()
+            self.device_values.append(int(data.rstrip()))
 
 
 if __name__ == '__main__':
