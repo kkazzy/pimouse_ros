7d74d02341949b1896fa6624f1dc67bfe3b12888
Kazuyuki Kawazu <knz_soft@yahoo.co.jp>
2018-02-04 19:12:48 +0900

    update travis_test_buzzer.py

--- test/travis_test_buzzer.py
+++ test/travis_test_buzzer.py
@@ -35,6 +35,18 @@ class BuzzerTest(unittest.TestCase):
         self.client.send_goal( goal, feedback_cb=self.feedback_cb)
         self.client.wait_for_result()
 
+        self.assertTrue(self.client.get_result(), "invalid result")
+        self.assertEqual(goal, freqs, self, device_values, "invalid feedback")
+              + "," .join([str(e) for e in self.device_values])
+
+        ##preemption##
+        self.device_values = []
+        self.client.send_goal( goal, feedback_cb=self.feedback_cb)
+        self.client.wait_for_result(rospy.Duration.from_sec(0.5))
+            
+        self.assertFalse(self.client.get_result(), "stop is requested but return true")
+        self.assertFalse(goal.freqs == self_device_values,  "not stoped")
+
 
     def feedback_cb(self):
         with open("/dev/rtbuzzer0", "r") as f:
